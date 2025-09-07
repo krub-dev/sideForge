1629636ac229513ce8e8772ce7569ff18882aca2
krub-dev <kikorubioillan@gmail.com>
2025-09-07 16:24:26 +0200

    Project created from initializer, added .gitignore

---
+++ src/main/java/com/sideforge/SideForgeApplication.java
@@ -0,0 +1,13 @@
+package com.sideforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideForgeApplication.class, args);
+	}
+
+}
